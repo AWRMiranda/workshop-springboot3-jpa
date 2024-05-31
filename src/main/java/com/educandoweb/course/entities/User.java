0399ffe6c89e164e0498bb2c95912aa15011b03c
AWRMiranda <alexandrewrm@gmail.com>
2024-05-31 08:53:00 -0300

    Order, Instant, ISO 8601

--- src/main/java/com/educandoweb/course/entities/User.java
+++ src/main/java/com/educandoweb/course/entities/User.java
@@ -1,17 +1,24 @@
 package com.educandoweb.course.entities;
 
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
+
 
 @Entity
 @Table(name = "tb_user")
 public class User implements Serializable {
 	private static final long serialVersionUID = 1L;
+
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
@@ -20,16 +27,20 @@ public class User implements Serializable {
 	private String phone;
 	private String password;
 
-	public User() {
+	@JsonIgnore
+	@OneToMany(mappedBy = "cliente")
+	private List<Order> orders = new ArrayList<>();
+	
+	public User() {	
 	}
-
+	
 	public User(Long id, String name, String email, String phone, String password) {
 		super();
 		this.id = id;
 		this.name = name;
 		this.email = email;
 		this.phone = phone;
-		this.password = password;
+		this.password = password;	
 	}
 
 	public Long getId() {
@@ -71,29 +82,41 @@ public class User implements Serializable {
 	public void setPassword(String password) {
 		this.password = password;
 	}
+		
+		@Override
+		public int hashCode() {
+			final int prime = 31;
+			int result = 1;
+			result = prime * result + ((id == null) ? 0 : id.hashCode());
+			return result;
+		}
 
-	@Override
-	public int hashCode() {
-		final int prime = 31;
-		int result = 1;
-		result = prime * result + ((id == null) ? 0 : id.hashCode());
-		return result;
-	}
-
-	@Override
+		@Override
 		public boolean equals(Object obj) {
-			if (this == obj)
+			if (this == obj) {
 				return true;
-			if (obj == null)
+			}
+			if (obj == null) {
 				return false;
-			if (getClass() != obj.getClass())
+			}
+			if (getClass() != obj.getClass()) {
 				return false;
+			}
 			User other = (User) obj;
 			if (id == null) {
-				if (other.id != null)
+				if (other.id != null) {
 					return false;
-			} else if (!id.equals(other.id))
+				}
+			} else if (!id.equals(other.id)) {
 				return false;
+			}
 			return true;
-	}
-}
+		}
+
+		
+		}
+
+	
+
+	
+	
