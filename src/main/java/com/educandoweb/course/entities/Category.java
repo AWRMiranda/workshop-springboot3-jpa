a20caeaf5b1dc8ff5386f149734e9f9e416ed772
AWRMiranda <alexandrewrm@gmail.com>
2024-06-02 16:38:17 -0300

    OrderItem, many-to-many association with extra attributes

--- src/main/java/com/educandoweb/course/entities/Category.java
+++ src/main/java/com/educandoweb/course/entities/Category.java
@@ -11,9 +11,10 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.ManyToMany;
-
+import jakarta.persistence.Table;
 
 @Entity
+@Table(name = "tb_category")	
 public class Category implements Serializable{
 	private static final long serialVersionUID = 1L;
 	
@@ -24,7 +25,6 @@ public class Category implements Serializable{
 	
 	@JsonIgnore
 	@ManyToMany(mappedBy = "categories")
-	
 	private Set<Product> products = new HashSet<>();
 	
 	public Category() {
--- src/main/java/com/educandoweb/course/entities/Order.java
+++ src/main/java/com/educandoweb/course/entities/Order.java
@@ -2,6 +2,8 @@ package com.educandoweb.course.entities;
 
 import java.io.Serializable;
 import java.time.Instant;
+import java.util.HashSet;
+import java.util.Set;
 
 import com.educandoweb.course.entities.enuns.OrderStatus;
 import com.fasterxml.jackson.annotation.JsonFormat;
@@ -12,6 +14,7 @@ import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 
 @Entity
@@ -30,6 +33,9 @@ private Integer OrderStatus;
 @ManyToOne
 @JoinColumn(name = "client_Id")
 private User client;
+
+@OneToMany(mappedBy = "id.order")
+private Set<OrderItem> items = new HashSet<>();
 	
 public Order() {
 		
@@ -77,7 +83,10 @@ public void setOrderStatus(OrderStatus orderStatus) {
 		this.client = client;	
 	}
 	
-	
+	public Set<OrderItem> getItems() {
+		return items;
+		
+	}
 
 	@Override
 	public int hashCode() {
